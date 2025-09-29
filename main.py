51bb8f916042111f54563c9f31d0dc93d1e7e285
annie-heng <annie.heng@edu.esiee.fr>
2025-09-29 13:33:25 +0000

    Premiere soumission de l'exercie sur la lecture de donnees

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+"""
+Module servant à lire un fichier csv contenant 
+uniquement des entiers.
+"""
 #### Imports et définition des variables globales
-
 FILENAME = "listes.csv"
 
 #### Fonctions secondaires
@@ -13,40 +16,59 @@ def read_data(filename):
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
+    with open(filename, mode = 'r', encoding = 'utf8') as f:
+        l = [[int(x) for x in line.split(";")] for line in f.readlines()]
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
+    """
+    Retourne la k+1ème ligne du fichier sous forme de liste.
+    """
+    return data[k]
 
 def get_first(l):
-    return None
+    """
+    Retourne le premier élement de la liste l.
+    """
+    return l[0]
 
 def get_last(l):
-    return None
+    """
+    Retourne le dernier élement de la liste l.
+    """
+    return l[-1]
 
 def get_max(l):
-    return None
+    """
+    Retourne le maximum de la liste l.
+    """
+    return max(l)
 
 def get_min(l):
-    return None
+    """
+    Retourne le minimum de la liste l.
+    """
+    return min(l)
 
 def get_sum(l):
-    return None
+    """
+    Retourne la somme des élements de la liste l.
+    """
+    return sum(l)
 
 
 #### Fonction principale
 
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
-
+    """
+    Fonction principale, affichant les lignes du fichier listes.csv
+    """
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
+    k = 37
+    print(k, get_list_k(data, 37))
 
 if __name__ == "__main__":
     main()
